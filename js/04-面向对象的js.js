2425da3c6d143bd75912ea6c176bf34625d4d0c8
胡乐 <huyue@Lenny-mpc.local>
2018-11-19 15:30:39 +0800

    添加继承和设置接收器笔记

--- js/04-面向对象的js.js
+++ js/04-面向对象的js.js
@@ -37,7 +37,7 @@ if (p && p.userBat && p.userBat()) {
   console.log('继承了userBat方法'); // 继承了userBat方法
 }
 
-// 实例属性比原型属性的优先级更高，查找属性时，先找实例本身，找不到时才到原型上去找
+// 实例属性比原型属性的优先级更高，查找属性时，先找实例本身，找不到时才到自身的原型上去找，当自身原型上找不到时，才会到父级上查找
 // 使用new关键字实例化时，是先将原型上的属性绑定到对象实例中，然后将构造函数中的属性（使用this.的属性）绑定到对象实例中
 // 在下面的例子中，this指的是new出来的对象实例p
 function Player() {
@@ -132,3 +132,148 @@ console.log(p2.text); // 我是原型上的属性
 Player.prototype.text = '我是修改后的原型属性';
 console.log(p1.text); // 我是修改后的原型属性
 console.log(p2.text); // 我是修改后的原型属性
+
+
+
+// 继承：继承是提高代码重用率的利器
+/**
+ * 要想有一个既没有副作用，又准确无误的继承机制，必须执行以下操作：
+ * 1.将原型设置成父类的实例来初始化原型链（继承）。这一步只需要做一个次（因为原型对象是共享的）
+ * 2.调用父类的构造函数来初始化对象自身。在每次实例化的时候都要这么做（每次构造对象的时候可以传入不同的参数）
+ */
+function Employee() {
+  this.name = '';
+  this.dept = 'None';
+  this.salary = '0.00';
+}
+
+function Manager() {
+  Employee.call(this); // 改变this的指当前构造函数实例化的对象
+  this.reports = [];
+}
+// 此处不使用new，而使用object.create，因为它可以在创建对象的时候指定原型，并且在不调用Employee构造函数的情况下，
+// 在父子之间创建和使用new操作符时一样的原型链。（这么创建出来的原型，修改原型上的属性或者方法时，不会影响Employee原型上的）
+Manager.prototype = Object.create(Employee.prototype);
+
+function IndividuaContributor() {
+  Employee.call(this);
+  this.active_projects = [];
+}
+IndividuaContributor.prototype = Object.create(Employee.prototype);
+
+function TeamLead() {
+  Manager.call(this);
+  this.dept = 'software';
+  this.salary = 100000;
+}
+TeamLead.prototype = Object.create(Manager.prototype);
+
+function Enginner() {
+  TeamLead.call(this);
+  this.dept = 'javascript';
+  this.desktop_id = '8822';
+  this.salary = 80000;
+}
+Enginner.prototype = Object.create(TeamLead.prototype);
+
+// 实例化对象
+// js处理new操作符时，会创建一个新的对象，然后将该对象作为this的值传给构造函数，并将_prototype_设置乘Class.prototype的值
+var genericEmployee = new Employee();
+console.log(genericEmployee); // Employee { name: '', dept: 'None', salary: '0.00' }
+
+var karen = new Manager(); // 实例化的时候，Manager继承了name，dept，salary一些默认的属性，reports是自身的
+console.log(karen); // Employee { name: '', dept: 'None', salary: '0.00', reports: [] }
+karen.name = 'karen';
+karen.reports = [1, 2, 3];
+console.log(karen);
+// Employee {
+//   name: 'karen',
+//   dept: 'None',
+//   salary: '0.00',
+//   reports: [ 1, 2, 3 ] }
+
+var jason = new TeamLead();
+jason.name = 'jason';
+console.log(jason); // Employee { name: 'jason', dept: 'software', salary: 100000, reports: {} }
+
+
+// 如果想要在运行时修改属性值，并希望改对象所有的后代都能继承这个值，就别把属性定义到构造函数中，而是定义到原型上
+function Employee() {
+  this.dept = 'None';
+  this.salary = 0;
+}
+Employee.prototype.name = '';
+
+function Manager() {
+  this.reports = [];
+}
+Manager.prototype = new Employee();
+var sandy = new Manager();
+var karen = new Manager();
+Employee.prototype.name = 'junk';
+console.log(sandy.name, karen.name); // junk  junk
+sandy.name = '123';
+console.log(sandy.name, karen.name, Employee.prototype.name, Manager.prototype.name); // 123 junk junk junk
+Manager.prototype.name = '从manager上修改';
+console.log(sandy.name, karen.name, Employee.prototype.name, Manager.prototype.name); // 123 从manager上修改 junk 从manager上修改
+// 修改子级原型上的不会影响父级原型上的，但是修改父级的会影响子级和所有实例化的对象（看起来好像是这样）
+// 实际的原因是因为，查找属性时，先找实例本身，找不到时才到自身的原型上去找，当自身原型上找不到时，才会到父级上查找，所以才
+// 会出现最后一句打印时，sandy.name为123，而karen.name却为“从manager上修改”，因为sandy.name被我赋值过，自身找到了，而karen.name是从原型上找到的
+
+
+// 接收器与设置器：接收器（getter）用来获取特定属性的值，设置器（setter）用来设置属性值的方法
+// 传统的写法：
+var person = {
+  firstname: 'x',
+  lastname: 'y',
+  setLastname: function(name) {
+    this.lastname = name;
+  },
+  setFirstname: function(name) {
+    this.firstname = name;
+  },
+  getFullname: function() {
+    return this.firstname + ' ' + this.lastname;
+  }
+}
+person.setFirstname('h');
+person.setLastname('l');
+console.log(person.getFullname()); // h l
+
+// es5默认写法
+var person = {
+  _firstname: 'x', // _firstname和 set, get后的名字不能同名
+  _lastname: 'y',
+  set lastname(name) {
+    this._lastname = name + '<2>';
+  },
+  set firstname(name) {
+    this._firstname = '<1>' + name;
+  },
+  get fullname() {
+    return this._firstname + ' ' + this._lastname;
+  }
+}
+person.lastname = '明';
+person.firstname = '小';
+console.log(person.fullname); //<1>小 明<2>
+
+// 写法2，使用object.defineProperty()，即使是对象已经创建后也能使用
+var person = {
+  firstname: 'x',
+  lastname: 'y'
+}
+Object.defineProperty(person, 'fullname', {
+  get: function() {
+    return this.firstname + ' ' + this.lastname;
+  },
+  set: function(name) {
+    let arr = name.split(' ');
+    this.firstname = arr[0];
+    this.lastname = arr[1];
+  }
+})
+person.fullname = '小 明';
+console.log(person.fullname); // 小 明
+
+// 工具库 lodash.js underecore.js
